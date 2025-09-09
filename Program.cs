ca3498c0f5c1b426d11d9920288ccf4043a75741
Erick Welberth <erickwelberth1@hotmail.com>
2025-09-09 20:17:49 -0300

    feat: finished project

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("start bank-account project");
+﻿using OOP_bank_account.Models;
+
+Console.WriteLine("start bank-account project: ");
+
+ContaCorrente conta = new ContaCorrente(1293094, 1000);
+
+conta.ExibirSaldo();
+conta.Sacar(500);
+conta.ExibirSaldo();
